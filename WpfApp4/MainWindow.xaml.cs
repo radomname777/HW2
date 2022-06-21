63c35b7ef45f80e989395c6172f30b3924135081
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-06-21 21:43:09 +0400

    0.4

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -25,31 +25,42 @@ namespace WpfApp4
         public MainWindow()
         {
             InitializeComponent();
+            TextRich.Document.Blocks.Clear();
         }
-
+        private void SaveFile()
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text Files|*.txt";
+            if (saveFile.ShowDialog() == true)
+            {
+                using StreamWriter sw = new StreamWriter(saveFile.FileName);
+                string richText = new TextRange(TextRich.Document.ContentStart, TextRich.Document.ContentEnd).Text;
+                sw.Write(richText);
+            }
+        }
+        private void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+                TextRich.Document.Blocks.Add(new Paragraph(new Run(File.ReadAllText(openFileDialog.FileName))));
+        }
+        private void CopyText()
+        {
+            TextRich.Copy();
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (sender is Button btn)
             {
                 switch (btn.Tag)
                 {
-                    case "Open":
-                        OpenFileDialog openFileDialog = new OpenFileDialog();
-                        openFileDialog.Filter = "Text Files|*.txt";
-                        if (openFileDialog.ShowDialog() == true)
-                        {
-                            TextRich.Document.Blocks.Add(new Paragraph(new Run(File.ReadAllText(openFileDialog.FileName))));
-                        }
-                        break;
-                    case "Save":
-                        SaveFileDialog saveFile = new SaveFileDialog();
-                        saveFile.Filter ="Text Files|*.txt";
-                        if (saveFile.ShowDialog() == true)
-                        {
-                            using StreamWriter sw = new StreamWriter(saveFile.FileName);
-                            sw.Write(TextRich.Document.Blocks);
-                        }
-                        break;
+                    case "Open":OpenFile();break;
+                    case "Save": SaveFile();break;
+                    case "Cut": TextRich.Cut(); break;
+                    case "Copy": TextRich.Copy(); break;
+                    case "Paste": TextRich.Paste(); break;
+                    case "Select": TextRich.SelectAll(); break;
                     default:
                         break;
                 }
