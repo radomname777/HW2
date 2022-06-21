0a2429827175a3bcabddb6b6b3146812b2ea6f31
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-06-21 20:30:43 +0400

    0.3

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,36 @@ namespace WpfApp4
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                switch (btn.Tag)
+                {
+                    case "Open":
+                        OpenFileDialog openFileDialog = new OpenFileDialog();
+                        openFileDialog.Filter = "Text Files|*.txt";
+                        if (openFileDialog.ShowDialog() == true)
+                        {
+                            TextRich.Document.Blocks.Add(new Paragraph(new Run(File.ReadAllText(openFileDialog.FileName))));
+                        }
+                        break;
+                    case "Save":
+                        SaveFileDialog saveFile = new SaveFileDialog();
+                        saveFile.Filter ="Text Files|*.txt";
+                        if (saveFile.ShowDialog() == true)
+                        {
+                            using StreamWriter sw = new StreamWriter(saveFile.FileName);
+                            sw.Write(TextRich.Document.Blocks);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+           
         }
     }
 }
